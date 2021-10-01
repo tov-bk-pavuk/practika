b2cbe53a5f6891c3877d4543803a310da06f0866
master <tov.bk.pavuk@gmail.com>
2021-10-01 17:14:03 +0300

    second commit

--- practika/settings.py
+++ practika/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p59(eo@$nulhxqhasxt*-g8$xq8t(*#y*s7z_u_)nbpt5t!po7'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-p59(eo@$nulhxqhasxt*-g8$xq8t(*#y*s7z_u_)nbpt5t!po7")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blog'
 ]
 
 MIDDLEWARE = [
